e428ae936ae37a513ac2a5cec18e4707b6719aa3
D®33DX <92141068+DreedX@users.noreply.github.com>
2022-05-23 03:16:00 -0400

    Create script-termux.sh
    Script para instalação Ubuntu.sh

---
+++ script-termux.sh
@@ -0,0 +1,18 @@
+#! /data/data/com.termux/files/usr/bin/bash
+
+termux-setup-storage
+
+apt-get update && apt-get upgrade -y
+
+apt install wget openssl-tool proot -y 
+
+wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh
+
+cd;ls 
+
+chmod +x ubuntu.sh 
+
+ls 
+
+bash ubuntu.sh 
+ 
